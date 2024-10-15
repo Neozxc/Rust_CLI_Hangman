70ad7bfb5c90a9fee802d3f9c555d049529128fe
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-10-15 20:41:46 +0100

    RUST | main() method

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,10 @@
 use rand::seq::SliceRandom;
 use std::io::{self, Write};
+
+fn main() {
+    let words: [&str; 5] = ["rustisgod", "programming", "language", "compiler", "ownership"];
+    let secret_word = words.choose(&mut rand::thread_rng()).unwrap();
+    let mut guesses = 6;
+    let mut guessed_letters = vec!["_", secret_word.len()];
+    let mut incorrect_guesses = Vec::new();
+}
