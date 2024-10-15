68441a843508192a6e71c8156c2f76ae0d69cefc
Ned Sakalauskis <62673820+Neozxc@users.noreply.github.com>
2024-10-15 20:50:51 +0100

    RUST | display_game_state method

--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,12 @@ fn main() {
     let mut guesses = 6;
     let mut guessed_letters = vec!["_", secret_word.len()];
     let mut incorrect_guesses = Vec::new();
+
+    println!("Welcome to the Hangman Game!");
+}
+
+fn display_game_state(guessed_letters: &Vec<char>, incorrect_guesses: &Vec<char>, guesses: usize) {
+    println!("\nWord {}", guessed_letters.iter().collect::<String>());
+    println!("Incorrect guesses: {:?}", incorrect_guesses);
+    println!("Guesses left: {}", guesses);
 }
